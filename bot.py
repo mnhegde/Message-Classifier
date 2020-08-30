9635765adda463635fc75514a4b80a044e4d8285
sec05 <58738123+sec05@users.noreply.github.com>
2020-08-29 23:43:43 -0400

    setting up for model intigration

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+import datetime
 import config
 #import model_creator as model
 client = discord.Client()
@@ -9,10 +10,13 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+
     if message.author == client.user:
         return
-
-    
+    if message.content.startswith('?help'):
+        await message.channel.send('If someone mentions someone I send the person whose mentioned a message about the priority of their mention!')
+        
+        
     fullMessageStart = str(message.content).split(">",1)
     newFullMessage = fullMessageStart[1]
     #uppercase letter percentage
@@ -37,15 +41,19 @@ async def on_message(message):
     else:
         avgWordLength = wordLength/len(words)
     print("average word length: " + str(wordLength))
+    print("number of words: "+str(len(words)))
     
     #time since last message 
     pastMessages = await message.channel.history(limit = 200).flatten()
     pastMessageTimes = []
-    print(len(message.raw_mentions))
     for mention in range(len(message.raw_mentions)):
         for msgs in range(len(pastMessages)):
-            if message.raw_mentions[mention] == pastMessages[msgs+1].author.id:
-                pastMessageTimes.append(pastMessages[msgs+1].created_at)
+            if message.raw_mentions[mention] == pastMessages[msgs].author.id:
+                pastMessageTimes.append(pastMessages[msgs].created_at)
+                if len(pastMessageTimes) == len(message.raw_mentions):
+                    break
+            else:
+                pastMessageTimes.append(datetime.datetime.now())
                 if len(pastMessageTimes) == len(message.raw_mentions):
                     break
         break
@@ -56,13 +64,17 @@ async def on_message(message):
         
     
     #running model and dm user
-    
+    thumbsUp = '\N{THUMBS UP SIGN}'
+    thumbsDown = '\N{THUMBS DOWN SIGN}'
+    await message.channel.send("React with thumbs up or down if my priority prediction was correct or not!")
+    await message.add_reaction(thumbsUp)
+    await message.add_reaction(thumbsDown)
     for member in message.mentions:
-        dm =  "You were mentioned in the server " + message.guild.name + " by " +str(member)+ " in the channel " + message.channel.name + " the message has a priority of ______"+"!"
-       # await 
+        [[pastMessageTimes[member]],[avgWordLength],[percentUppercaseLetters],[len(words)]]
+        dm =  "You were mentioned in **"+  message.channel.name + "** on server **" + message.guild.name + "** by **" +str(member)+  "** the message has a priority of **"+"**!"
         await discord.User.send(member,dm)
+        
 
-    if message.content.startswith('?help'):
-        await message.channel.send('If someone mentions someone I send the person whose mentioned a message about the priority of their mention!')
+
 
 client.run(config.BOT_TOKEN)
